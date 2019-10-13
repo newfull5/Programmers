a7d9cead038a2dc25c83b552a7c885ecf5dc7218
newfull5 <52832716+newfull5@users.noreply.github.com>
2019-10-13 17:09:33 +0900

    Create JadenCase 문자열 만들기.py

---
+++ JadenCase 문자열 만들기.py
@@ -0,0 +1,14 @@
+'''
+def solution(s):
+    s = s.lower()
+    s = s.replace(s[0],s[0].upper(),1)
+   
+    for i in range(1,len(s)-1):
+        if s[i] == ' ':
+            s = s.replace(s[i+1],s[i+1].upper(),1)
+
+    return s
+    
+    이게 안되는 이유를 모르겠다
+'''
+
