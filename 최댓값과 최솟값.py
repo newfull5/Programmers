01a345097cea74cc677d1c061d81e5d199e2de9a
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-02-14 13:31:59 +0900

    Update 최댓값과 최솟값.py

--- 최댓값과 최솟값.py
+++ 최댓값과 최솟값.py
@@ -11,3 +11,12 @@ def solution(s):
     k = "{} {}".format(answer[0],answer[-1])
     
     return k
+
+--------------------
+2020.02.14
+
+def solution(s):
+    temp = s.split()
+    temp = list(map(int,temp))
+    
+    return '{} {}'.format(min(temp),max(temp))
