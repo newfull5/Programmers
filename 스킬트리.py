280701fe27d35d2d27f55df5ba80dcf31437322e
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-02-23 15:11:21 +0900

    Update 스킬트리.py

--- 스킬트리.py
+++ 스킬트리.py
@@ -1,3 +1,29 @@
+'''
+2020.02.23
+def solution(skill, skill_trees):
+    right = []
+    stack = []
+    cnt = 0
+    
+    for i in range(0, len(skill)+1):
+        right.append(skill[:i])
+        
+    skill = list(skill)    
+    
+    for each in skill_trees:
+        string = ''
+        for i in range(0,len(each)):
+            if each[i] in skill:
+                string += each[i]
+                
+        stack.append(string)
+        
+    for num in stack:
+        if num in right:
+            cnt += 1
+            
+    return cnt
+'''
 def solution(skill, skill_trees):
     answer = []
     for i in range(0, len(skill)):
