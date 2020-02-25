6b230675f054a2eb1b70515224886336d87a5daa
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-02-25 14:50:21 +0900

    Create 가장 큰 수.py

---
+++ 가장 큰 수.py
@@ -0,0 +1,25 @@
+'''
+
+def solution(numbers):
+    hashh = []
+    answer = ''
+
+    maxlen = len(str(max(numbers)))
+
+    for num in numbers:
+        hashh.append([num,int(str(num)+((maxlen-len(str(num)))*str(num)[-1]))])
+
+    hashh = sorted(hashh,key=lambda a: a[1], reverse = True)
+
+    for has in hashh:
+        answer += str(has[0])    
+
+    return answer
+    # 이게 왜 안되는지 모르겠다 아래랑 같은 방식인데 뭐지...
+'''
+# 문자열의 대소 비교를 이용한 방법
+
+def solution(numbers):
+    numbers = list(map(str,numbers))
+    numbers = sorted(numbers,key=lambda a: a*3, reverse = True)
+    return str(int(''.join(numbers)))
