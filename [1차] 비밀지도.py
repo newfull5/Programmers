53b4afcc3a695545f09822f265cc61f36708d6cb
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-02-26 16:51:11 +0900

    Update [1차] 비밀지도.py

--- [1차] 비밀지도.py
+++ [1차] 비밀지도.py
@@ -1,3 +1,4 @@
+'''
 def solution(n, arr1, arr2):
     answer1 = []
     answer2 = []
@@ -24,4 +25,34 @@ def solution(n, arr1, arr2):
                 temp += ' '
 
         answer.append(temp)
+    return answer
+'''
+# 2020.02.26
+# 20일전 풀이에서 조금도 달라진게 없다. 풀이가 하나밖에 없는 문제인건가? 아니면 성장하지 못한 것인가?
+def solution(n, arr1, arr2):
+    ar1 = []
+    ar2 = []
+    answer = []
+    
+    for num in arr1:
+        if len(bin(num)[2:]) != n:
+            ar1.append('0'*(n - len(bin(num)[2:])) + bin(num)[2:])
+        else:
+            ar1.append(bin(num)[2:])
+            
+    for num in arr2:
+        if len(bin(num)[2:]) != n:
+            ar2.append('0'*(n - len(bin(num)[2:])) + bin(num)[2:])
+        else:
+            ar2.append(bin(num)[2:])
+            
+    for i in range(0, n):
+        string = ''
+        for j in range(0, n):
+            if ar1[i][j] == '1' or ar2[i][j] == '1':
+                string += '#'
+            else:
+                string += ' '
+        answer.append(string)
+        
     return answer
