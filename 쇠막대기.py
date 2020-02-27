2416b2ae46aa87ed90713f95b7359f5b18f66a50
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-02-27 14:41:42 +0900

    Update 쇠막대기.py

--- 쇠막대기.py
+++ 쇠막대기.py
@@ -1,3 +1,4 @@
+'''
 def solution(arrangement):
     cnt = 0
     arrangement = list(arrangement.replace('()','r'))
@@ -16,4 +17,21 @@ def solution(arrangement):
         if not '(' in arrangement:
             break
             
+    return cnt
+
+'''
+#2020.02.07
+#확실히 4달 전보다 성장하긴 했으나 시간 내에 풀 자신은 없다. 아직도 나는 한없이 작다.
+def solution(arrangement):
+    stack = []
+    arrangement = arrangement.replace('()','r')
+    
+    cnt = 0
+    for i in range(0, len(arrangement)):
+        if arrangement[i] == '(':
+            stack.append(i)
+            
+        if arrangement[i] == ')':
+            cnt += arrangement[stack[-1]:i].count('r') + 1
+            stack.pop()
     return cnt
