8c189043c4d6ee7e29a9f15735a05511a8c6c72d
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-03-01 00:17:44 +0900

    Create 124 나라의 숫자.py

---
+++ 124 나라의 숫자.py
@@ -0,0 +1,18 @@
+#역시 재귀는 어렵다.
+
+def solution(a):
+    answer = ''
+    def div(a):
+        nonlocal answer
+        if a == 0:
+            return answer[::-1]
+        if a % 3 == 0:
+            answer += '4'
+            return div(a//3 - 1)
+        if a % 3 == 1:
+            answer += '1'
+            return div(a//3)
+        if a % 3 == 2:
+            answer += '2'
+            return div(a//3)
+    return div(a)
