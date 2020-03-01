42adcb0c7fac37a65d0dd848f3b9125d51462db6
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-03-01 15:58:30 +0900

    Update 라면공장.py

--- 라면공장.py
+++ 라면공장.py
@@ -1,3 +1,4 @@
+'''
 def solution(stock, dates, supplies, k):
     s = 0
     cnt = 0
@@ -16,4 +17,23 @@ def solution(stock, dates, supplies, k):
         stock = stock + s
         cnt += 1
     
+    return cnt
+'''
+#힙을 사용하면 수행시간이 많이 줄어드는건 알겠습니다만... 언제 힙을 사용해야하는지 바로보고 판단할 자신이 없다.
+#2020.03.01
+import heapq
+
+def solution(stock, dates, supplies, k):
+    heap = []
+    cnt = 0
+    idx = 0
+    while stock < k:
+        for i in range(idx,len(supplies)):
+            if dates[idx] > stock:
+                break
+            heapq.heappush(heap,(-supplies[i],supplies[i]))
+            idx += 1
+        stock += heapq.heappop(heap)[1]
+        cnt += 1
+        
     return cnt
