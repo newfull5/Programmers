da5f8dcd2f43db8037071f707a1f029f820422af
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-03-04 19:17:43 +0900

    Update N개의 최소공배수.py

--- N개의 최소공배수.py
+++ N개의 최소공배수.py
@@ -1,3 +1,4 @@
+'''
 def gcd(a,b):
     a,b=max(a,b),min(a,b)
     while b > 0:
@@ -11,3 +12,21 @@ def solution(arr):
         arr.insert(0,int(a*b/c))
     answer=arr[0]
     return answer
+'''
+#2020.03.04
+def gcd(a,b):
+    if a % b == 0:
+        return b
+    return gcd(b,a%b)
+
+def chlthrhd(arr):
+    if len(arr) == 1:
+        return arr[0]
+    a = arr.pop()
+    b = arr.pop()
+    chleorhd = gcd(a,b)
+    arr.append(a*(b // chleorhd))
+    return chlthrhd(arr)
+
+def solution(arr):
+    return chlthrhd(arr)
