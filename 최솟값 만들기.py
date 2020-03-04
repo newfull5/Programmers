12ab93f0ec9ad822fc844b1b486c06ede3b29cb1
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-03-04 19:29:58 +0900

    Update 최솟값 만들기.py

--- 최솟값 만들기.py
+++ 최솟값 만들기.py
@@ -1,3 +1,4 @@
+'''
 def solution(A,B):
     A.sort()
     B = sorted(B,reverse = True)
@@ -6,3 +7,13 @@ def solution(A,B):
         temp.append(A[i]*B[i])
         
     return sum(temp)
+'''
+#2020.03.04
+def solution(A,B):
+    A.sort()
+    B.sort(reverse = True)
+
+    temp = 0
+    for i in range(0, len(A)):
+        temp += A[i]*B[i]
+    return temp
