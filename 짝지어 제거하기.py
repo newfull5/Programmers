26d3f317d486cb727db1b6474d412b9c94fc3516
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-03-04 17:48:57 +0900

    Update 짝지어 제거하기.py

--- 짝지어 제거하기.py
+++ 짝지어 제거하기.py
@@ -1,3 +1,4 @@
+'''
 def solution(s):
     result = []
     for i in s:
@@ -8,3 +9,27 @@ def solution(s):
         else:
             result.append(i)
     return int(not bool(result))
+'''
+#2020.03.04
+import collections
+
+def solution(s):
+    s = list(s)
+    while True:
+        length = len(s)
+        stack = collections.deque([])
+        if not stack and not s:
+            return 1
+        for num in s:
+            if len(stack) == 0:
+                stack.append(num)
+                continue
+            if num == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(num)
+        s = list(stack)
+        if length == len(s):
+            return 0
+
+
