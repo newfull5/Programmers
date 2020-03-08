0e10295e5076fd9cfa47b59cf80ff43e2330c903
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-03-08 18:36:45 +0900

    Create 야근 지수.py

---
+++ 야근 지수.py
@@ -0,0 +1,22 @@
+#보자마자 힙으로 푸는 문제구나 하고 깨닳았다. 뭔가 조금은 성장한 느낌이다.
+
+from heapq import heappop, heappush
+
+def solution(n, works):
+    if sum(works) <= n:
+        return 0
+    
+    heap = []
+    for num in works:
+        heappush(heap,-num)
+        
+    n *= -1
+    while n != 0:
+        heappush(heap,(heappop(heap) + 1))
+        n += 1
+    
+    answer = 0
+    for i in heap:
+        answer += i**2
+    
+    return answer
