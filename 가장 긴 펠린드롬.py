b52f13ff404798c7f1747d16b81cb701ef5d986a
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-03-08 17:58:15 +0900

    Update 가장 긴 펠린드롬.py

--- 가장 긴 펠린드롬.py
+++ 가장 긴 펠린드롬.py
@@ -1,3 +1,4 @@
+'''
 def solution(s):
     answer = []
     
@@ -7,4 +8,13 @@ def solution(s):
             
             if temp_str == temp_str[::-1]:
                 answer.append(len(temp_str))
+    return max(answer)
+'''
+#2020.03.08
+def solution(s):
+    answer = []
+    for i in range(len(s)):
+        for j in range(i+1, len(s)+1):
+            if s[i:j] == s[i:j][::-1]:
+                answer.append(j-i)
     return max(answer)
