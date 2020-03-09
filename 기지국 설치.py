43b1147de790ec63d12c7189c83431215d8490f6
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-03-09 16:47:54 +0900

    Update 기지국 설치.py

--- 기지국 설치.py
+++ 기지국 설치.py
@@ -1,3 +1,4 @@
+'''
 import math
 
 def solution(N, stations, W):
@@ -11,4 +12,18 @@ def solution(N, stations, W):
     if breakpoint < N:
         ans += math.ceil((N-breakpoint)/(1+(2*W)))
 
+    return ans
+'''
+#동시에 이렇게도 풀어 보았다. 아래의 방법으로 푸는 것이 실수가 적을듯 하다.
+import math
+
+def solution(N, stations, W):
+    stations = [W*-1] + stations
+    stations.append(N+W+1)
+    ans = 0
+    
+    for i in range(0,len(stations)-1):
+        if stations[i+1] - stations[i] > W*2:
+            ans += math.ceil(((stations[i+1] - stations[i]) - (1+W*2))/(1+(2*W)))
+            
     return ans
