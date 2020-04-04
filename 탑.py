50b53b7ccab5f881a3539f5f202077e9c37a298a
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-04-04 12:47:50 +0900

    Update 탑.py

--- 탑.py
+++ 탑.py
@@ -1,3 +1,4 @@
+'''
 def solution(heights):
     answer = []
     
@@ -11,4 +12,21 @@ def solution(heights):
                 break
                 
     return answer
+'''
+
+#2020.04.04
+def solution(heights):
+    answer = []
     
+    for i in range(len(heights)-1,-1,-1):
+        for j in range(i-1,-1,-1):
+            if heights[i] < heights[j]:
+                answer.append(j+1)
+                break
+            if j == 0:
+                answer.append(0)
+                break
+
+    answer.append(0)
+    
+    return list(reversed(answer))
