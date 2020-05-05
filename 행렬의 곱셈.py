ff3d389c6a0ac9423a4aed1b03ac2c7c2ffa2a47
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-05-05 13:03:07 +0900

    Create 행렬의 곱셈.py

---
+++ 행렬의 곱셈.py
@@ -0,0 +1,16 @@
+#2020.05.05
+def solution(arr1, arr2):
+    arr3 = list(map(list, zip(*arr2)))
+    answer = []
+    
+    for i in range(0, len(arr1)):
+        arr = []    
+        for j in range(0, len(arr2[0])):
+            temp = 0
+            for k in range(0, len(arr3[0])):
+                temp += arr1[i][k]*arr3[j][k]
+
+            arr.append(temp)
+        answer.append(arr)
+    
+    return answer
