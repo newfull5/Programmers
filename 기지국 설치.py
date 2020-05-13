4d337ea0197d00ea338c4228124fc6fdd63973cc
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-05-13 19:04:30 +0900

    Update 기지국 설치.py

--- 기지국 설치.py
+++ 기지국 설치.py
@@ -14,6 +14,7 @@ def solution(N, stations, W):
 
     return ans
 '''
+'''
 #동시에 이렇게도 풀어 보았다. 아래의 방법으로 푸는 것이 실수가 적을듯 하다.
 import math
 
@@ -27,3 +28,19 @@ def solution(N, stations, W):
             ans += math.ceil(((stations[i+1] - stations[i]) - (1+W*2))/(1+(2*W)))
             
     return ans
+'''
+#2020.05.13
+import math
+
+def solution(N, stations, W):
+    checkpoint = 0
+    cnt = 0
+
+    for a in stations:
+        cnt += math.ceil(((a-W)-1 - checkpoint)/((W*2)+1))
+        checkpoint = a+W
+
+    if (N - checkpoint) > 0:
+        cnt += math.ceil((N - checkpoint)/((W*2)+1))
+    
+    return cnt
