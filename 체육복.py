0ec802815a7e816432856339f27fb3dfd5d0f65d
newfull5 <52832716+newfull5@users.noreply.github.com>
2020-05-27 23:57:36 +0900

    Update 체육복.py

--- 체육복.py
+++ 체육복.py
@@ -1,3 +1,4 @@
+'''
 def solution(n, lost, reserve):
     _reserve = [r for r in reserve if r not in lost]
     _lost = [l for l in lost if l not in reserve]
@@ -10,3 +11,19 @@ def solution(n, lost, reserve):
         elif b in _lost:
             _lost.remove(b)
     return n - len(_lost)
+'''
+#2020.05.27
+def solution(n, lost, reserve):
+    
+    for res in reserve[:]:
+        if res in lost:
+            lost.remove(res)
+            reserve.remove(res)
+            
+    for res in reserve:
+        if res-1 in lost:
+            lost.remove(res-1)
+        elif res+1 in lost:
+            lost.remove(res+1)
+            
+    return n-len(lost)
