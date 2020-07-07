299feb05ed125e91708672097a24e6dde0627320
Sae_Chan_Oh <dhtocks@naver.com>
2020-07-07 20:53:48 +0900

    Update K번째수.py

--- K번째수.py
+++ K번째수.py
@@ -6,7 +6,7 @@ def solution(array, commands):
     for t in range(0, len(commands)):
         i = commands[t][0]
         j = commands[t][1]
-        k = commands[t][2]
+        k = commands[t][2] 
 
         arr = array[i-1:j]
         arr.sort()
