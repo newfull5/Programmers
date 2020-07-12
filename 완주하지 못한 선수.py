c03ce1fb318d87bb491c10582524531537a016e4
Sae_Chan_Oh <dhtocks@naver.com>
2020-07-13 07:16:24 +0900

    Update 완주하지 못한 선수.py

--- 완주하지 못한 선수.py
+++ 완주하지 못한 선수.py
@@ -1,3 +1,4 @@
+'''
 def solution(participant, completion):
     participant.sort()
     completion.sort()
@@ -5,4 +6,16 @@ def solution(participant, completion):
     for i in range(0, len(completion)):
         if participant[i] != completion[i]:
             return participant[i]
+    return participant[-1]
+'''
+#2020.07.13
+#와 맙소사 톳씨하나 안틀리고 완전히 똑같다. 뭐지...
+def solution(participant, completion):
+    participant.sort()
+    completion.sort()
+
+    for i in range(0, len(completion)):
+        if participant[i] != completion[i]:
+            return participant[i]
+        
     return participant[-1]
