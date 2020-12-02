8d00a55041be881ff26a0dd29fff61ee579ce262
Sae_Chan_Oh <dhtocks@naver.com>
2020-12-02 15:10:15 +0900

    Update 더 맵게.py

--- 더 맵게.py
+++ 더 맵게.py
@@ -15,7 +15,7 @@ def solution(scoville, K):
         elif scoville[0] > K:
             return cnt
 '''
-
+"""
 #2020.02.26
 import heapq
 
@@ -29,3 +29,26 @@ def solution(scoville, K):
         heapq.heappush(scoville, (heapq.heappop(scoville)+heapq.heappop(scoville)*2))
         
     return cnt
+
+"""
+#2020.12.02
+import heapq
+
+def solution(scoville, K):
+    cnt = 0
+    
+    heapq.heapify(scoville)
+    
+    while True:
+        first = heapq.heappop(scoville)
+        second = heapq.heappop(scoville)
+
+        heapq.heappush(scoville, (first + second*2))
+
+        cnt += 1
+        
+        if scoville[0] >= K:
+            return cnt
+        
+        if len(scoville) == 1:
+            return -1
