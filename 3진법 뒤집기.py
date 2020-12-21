822ac92587db62aa44f0a8edc80cd7886314a512
Sae_Chan_Oh <dhtocks@naver.com>
2020-12-21 16:46:08 +0900

    Update 3진법 뒤집기.py

--- 3진법 뒤집기.py
+++ 3진법 뒤집기.py
@@ -1,3 +1,5 @@
+"""
+#2020.10.13
 def convert(n, base):
     T = "0123456789ABCDEF"
     q, r = divmod(n, base)
@@ -8,3 +10,17 @@ def convert(n, base):
     
 def solution(n):
     return int(convert(n, base=3)[::-1], 3)
+"""
+#2020.12.21
+def convert(n,base):
+    
+    nomi, denomi = divmod(n,base)
+
+    if nomi == 0:
+        return str(denomi)
+    else:
+        return convert(nomi, base) + str(denomi)
+    
+def solution(n):
+
+    return int(convert(n,3)[::-1], 3)
