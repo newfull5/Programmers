ed8a07039f68ea364992b620d305f86be3a7488b
Sae_Chan_Oh <dhtocks@naver.com>
2020-12-28 10:37:39 +0900

    Update 가장 큰 수.py

--- 가장 큰 수.py
+++ 가장 큰 수.py
@@ -17,9 +17,16 @@ def solution(numbers):
     return answer
     # 이게 왜 안되는지 모르겠다 아래랑 같은 방식인데 뭐지...
 '''
+"""
 # 문자열의 대소 비교를 이용한 방법
 
 def solution(numbers):
     numbers = list(map(str,numbers))
     numbers = sorted(numbers,key=lambda a: a*3, reverse = True)
     return str(int(''.join(numbers)))
+"""
+
+def solution(numbers):
+    numbers = list(map(str, numbers))
+    return (str(int(''.join(sorted(numbers, key = lambda x: x*3 , reverse = True)))))
+    
