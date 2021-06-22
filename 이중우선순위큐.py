31b722a660426561b465b095cfa754366ce22147
Sae_Chan_Oh <dhtocks@naver.com>
2021-06-23 08:51:19 +0900

    Update 이중우선순위큐.py

--- 이중우선순위큐.py
+++ 이중우선순위큐.py
@@ -1,3 +1,4 @@
+'''
 #힙을 사용해야 풀리는 , 문제의 케이스가 너무 적어서 이런식으로도 풀리더라.
 def solution(operations):
     stack = []
@@ -14,3 +15,23 @@ def solution(operations):
     if not stack:
         return [0,0]
     return [max(stack),min(stack)]
+'''
+#2021.06.23
+def solution(operations):
+    queue = []
+
+    while operations:
+        operation = operations.pop(0)
+
+        if queue and operation == "D 1":
+            queue.remove(max(queue))
+
+        elif queue and operation == "D -1":
+            queue.remove(min(queue))
+
+        else:
+            queue.append(int(operation[1:]))
+
+    if len(queue) > 1:
+        return [max(queue), min(queue)]
+    return [0,0]
