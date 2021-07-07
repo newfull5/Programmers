05f0ed78aa2b299f47decd3ed13f1cca1332b17f
Sae_Chan_Oh <dhtocks@naver.com>
2021-07-07 10:41:46 +0900

    Update 카펫.py

--- 카펫.py
+++ 카펫.py
@@ -7,9 +7,20 @@ def solution(brown, red):
         if i * (brown - i) == red:
             return [(brown-i)+2,i+2]
 '''
+"""
 #2020.03.01
 def solution(brown, red):
     enffp = (brown - 4)//2
     for i in range(enffp):
         if (i * (enffp - i)) == red:
             return [(enffp-i)+2,i+2]
+"""
+   
+#2021.07.07
+def solution(brown, yellow):
+    brown -= 4
+    brown //= 2
+
+    for i in range(1, brown//2 +1):
+        if ((brown-i) * i) == yellow:
+            return [brown-i + 2, i + 2]
