6c1d2dce991df3921cfc5fc711f6a4195128aa86
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-09 00:12:08 +0900

    Update 약수의 개수와 덧셈.py

--- 약수의 개수와 덧셈.py
+++ 약수의 개수와 덧셈.py
@@ -1,3 +1,4 @@
+"""
 def solution(left, right):
     
     answer = 0
@@ -12,6 +13,25 @@ def solution(left, right):
     for num in range(left,right+1):
         n = divisor(num)
         if n % 2 == 0:
+            answer += num
+        else:
+            answer -= num
+            
+    return answer
+"""
+#2022.11.08
+def get_denonum(number):
+    answer = 0
+    for num in range(1, number+1):
+        if number % num == 0:
+            answer += 1
+    return answer
+
+def solution(left, right):
+    answer = 0
+    for num in range(left, right+1):
+        denominator_num = get_denonum(num)
+        if denominator_num % 2 == 0:
             answer += num
         else:
             answer -= num
