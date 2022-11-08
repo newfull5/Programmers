25e851ff87051ffc83d3479464a0b45eadfb9cee
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-09 00:02:38 +0900

    Update 부족한 금액 계산하기.py

--- 부족한 금액 계산하기.py
+++ 부족한 금액 계산하기.py
@@ -1,3 +1,4 @@
+"""
 def solution(price, money, count):
     cost = price * (count * (count + 1)) // 2
 
@@ -15,3 +16,13 @@ if __name__ == '__main__':
     money = 20
     count = 4
     print(solution(price, money, count))
+    
+"""
+
+# 2022.11.08
+def solution(price, money, count):
+    answer = (price + count*price)*count/2 - money
+    
+    if answer < 0:
+        return 0
+    return answer
