da26b19a1ab434fbd2cc776bd14aa5aa405e4dc0
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-08 23:06:48 +0900

    Update 나머지가 1이 되는 수 찾기.py

--- 나머지가 1이 되는 수 찾기.py
+++ 나머지가 1이 되는 수 찾기.py
@@ -1,3 +1,4 @@
+"""
 def soinsu_boonhae(num):
     for i in range(2, num):
         if num % i == 0:
@@ -10,3 +11,10 @@ def solution(n):
         if num == soinsu_boonhae(num):
             return num
         num = soinsu_boonhae(num)
+"""
+#2022.11.08
+
+def solution(n):
+    for num in range(1, n):
+        if n % num == 1:
+            return num
