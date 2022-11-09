d9f8f034b45cfd0f6ae08e7d19a4b4be77e66e5e
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-09 18:42:36 +0900

    Update 음양 더하기.py

--- 음양 더하기.py
+++ 음양 더하기.py
@@ -1,2 +1,8 @@
+"""
 def solution(absolutes, signs):
     return sum([absolutes[i] for i in range(len(absolutes)) if signs[i]]) - sum([absolutes[i] for i in range(len(absolutes)) if not signs[i]])
+"""
+#2022.11.09
+
+def solution(absolutes, signs):
+    return sum([num if sign else num*-1 for num, sign in zip(absolutes, signs)])
