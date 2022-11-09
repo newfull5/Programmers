a0d05b8e0e4dc49da81ed4a111f09e4b197c08a3
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-09 20:43:09 +0900

    Update 두 개 뽑아서 더하기.py

--- 두 개 뽑아서 더하기.py
+++ 두 개 뽑아서 더하기.py
@@ -9,11 +9,17 @@ def solution(numbers):
     return sorted(list(set(answer)))
 """
 
+"""
 # 2020.10.07
-
 def solution(numbers):
     answer = []
     for i in range(len(numbers)):
         for j in range(i+1,len(numbers)):
             answer.append(numbers[i]+numbers[j])
     return sorted(list(set(answer)))
+"""
+#2022.11.09
+from itertools import combinations
+
+def solution(numbers):
+    return sorted(list(set(sum(nums) for nums in combinations(numbers, 2))))
