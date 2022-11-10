8bf68079fb4808d685f31959e5b99c3243c4f70b
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-10 22:38:50 +0900

    Update 크레인 인형뽑기 게임.py

--- 크레인 인형뽑기 게임.py
+++ 크레인 인형뽑기 게임.py
@@ -1,3 +1,4 @@
+"""
 def solution(board, moves):
     stack = []
     answer = 0
@@ -16,4 +17,31 @@ def solution(board, moves):
                         answer += 2
                 break
                 
+    return answer
+"""
+import numpy as np
+from collections import deque
+
+
+def solution(board, moves):
+    stack = [-1, 0]
+    answer = 0
+
+    board = np.rot90(np.array(board)).tolist()
+    board = [deque(li) for li in board][::-1]
+
+    for idx in moves:
+        if not board[idx - 1]:
+            continue
+
+        while board[idx - 1][0] == 0:
+            board[idx - 1].popleft()
+
+        stack.append(board[idx - 1].popleft())
+
+        while stack[-1] == stack[-2]:
+            stack.pop()
+            stack.pop()
+            answer += 2
+
     return answer
