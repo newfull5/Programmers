e595cc7ade4a6c2f6822da0ff11c4dd6bd7c76e9
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-11 18:51:43 +0900

    Update 체육복.py

--- 체육복.py
+++ 체육복.py
@@ -29,6 +29,7 @@ def solution(n, lost, reserve):
             
     return n-len(lost)
 '''
+'''
 #2020.10.17
 def solution(n, lost, reserve):
     lost_copy = lost[:]
@@ -45,3 +46,33 @@ def solution(n, lost, reserve):
             lost.remove(res+1)
 
     return (n - len(lost))
+'''
+#2022.11.11
+def run_ltr(n, lost, reserve):
+    for student in (set(lost) & set(reserve)):
+        lost.remove(student)
+        reserve.remove(student)
+    
+    for res in reserve:
+        if res-1 in lost:
+            lost.remove(res-1)
+        elif res+1 in lost:
+            lost.remove(res+1)
+            
+    return n-len(lost)    
+    
+def run_rtl(n, lost, reserve):
+    for student in (set(lost) & set(reserve)):
+        lost.remove(student)
+        reserve.remove(student)
+        
+    for res in reserve:
+        if res+1 in lost:
+            lost.remove(res+1)
+        elif res-1 in lost:
+            lost.remove(res-1)
+            
+    return n-len(lost)  
+
+def solution(n, lost, reserve):
+    return max(run_rtl(n,lost[:],reserve[:]), run_ltr(n,lost[:],reserve[:]))
