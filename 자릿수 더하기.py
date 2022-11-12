2d9d4024eba62d36b69fb919a0397159e2ed28e1
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-12 23:53:38 +0900

    Update 자릿수 더하기.py

--- 자릿수 더하기.py
+++ 자릿수 더하기.py
@@ -2,7 +2,11 @@
 def solution(n):
     return sum(list(map(int,str(n))))
 """
-
+"""
 # 2022.11.03
+def solution(n):
+    return sum(map(int, list(str(n))))
+"""
+#2022.11.12
 def solution(n):
     return sum(map(int, list(str(n))))
