f0aa43e798761555df391e3d2d87401e907ad526
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-12 23:48:38 +0900

    Update 정수 제곱근 판별.py

--- 정수 제곱근 판별.py
+++ 정수 제곱근 판별.py
@@ -1,7 +1,14 @@
+"""
 def solution(n):
 
     for i in range(10000001):
         if i*i == n:
             return (i+1)*(i+1)
 
+    return -1
+"""
+#2022.11.12
+def solution(n):
+    if n ** (1/2) == int(n ** (1/2)):
+        return (n**(1/2) +1)**2
     return -1
