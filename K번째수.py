16e68e3d12e699d25260883a8b8c715d197b73df
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-12 17:13:30 +0900

    Update K번째수.py

--- K번째수.py
+++ K번째수.py
@@ -39,6 +39,12 @@ def solution(array, commands):
         
     return answer
 '''
+"""
 #2021.06.23
 def solution(array, commands):
     return [sorted(array[a-1:b])[c-1] for a,b,c in commands]
+"""
+
+#2022.11.12
+def solution(array, commands):
+    return [sorted(array[i-1:j])[k-1] for i,j,k in commands]
