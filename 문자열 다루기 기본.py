bcf06afbdda4db4821066c20f11daa8b0c84b157
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-13 01:12:34 +0900

    Update 문자열 다루기 기본.py

--- 문자열 다루기 기본.py
+++ 문자열 다루기 기본.py
@@ -1,2 +1,13 @@
+"""
 def solution(s):
     return s.isnumeric()
+"""
+#2022.11.12
+import re
+
+def solution(s):
+    if len(s) == 4 or len(s) == 6:
+        if re.search('[a-zA-Z]', s):
+            return False
+        return True
+    return False
