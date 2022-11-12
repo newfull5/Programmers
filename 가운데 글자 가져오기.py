6fa0eb8d439a635faba5e79f87ea415a670871ab
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-13 01:43:18 +0900

    Update 가운데 글자 가져오기.py

--- 가운데 글자 가져오기.py
+++ 가운데 글자 가져오기.py
@@ -1,5 +1,12 @@
+"""
 def solution(s):
     if len(s) % 2 == 1:
         return s[(len(s)//2)]
     else:
         return s[(len(s)//2)-1:(len(s)//2)+1]
+"""
+#2022.11.13
+def solution(s):
+    if len(s) % 2 == 0:
+        return s[(len(s)//2)-1:(len(s)//2)+1]
+    return s[len(s)//2]
