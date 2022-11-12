1fc35e62be4e6dd45bfc320e0375b284026ba82a
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-12 23:52:39 +0900

    Update 자연수 뒤집어 배열로 만들기.py

--- 자연수 뒤집어 배열로 만들기.py
+++ 자연수 뒤집어 배열로 만들기.py
@@ -1,3 +1,4 @@
+"""
 def solution(n):
     n = list(map(int,str(n)))
     answer = []
@@ -6,3 +7,7 @@ def solution(n):
         answer.append(n[-i])
 
     return answer
+"""
+#2022.11.12
+def solution(n):
+    return [int(v) for v in str(n)[::-1]]
