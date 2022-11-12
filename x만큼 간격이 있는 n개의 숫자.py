ecf1f1caada1c7010a907b83e75c9db5eba353f2
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-12 23:16:23 +0900

    Update x만큼 간격이 있는 n개의 숫자.py

--- x만큼 간격이 있는 n개의 숫자.py
+++ x만큼 간격이 있는 n개의 숫자.py
@@ -1,3 +1,4 @@
+"""
 def solution(x, n):
     abc = []
     
@@ -5,4 +6,9 @@ def solution(x, n):
         abc.append(i*x)
         
     return abc
- 
+"""
+#2022.11.12 
+def solution(x, n):
+    if x == 0:
+        return [x]*n
+    return [v for v in range(x,x*(n+1),x)]
