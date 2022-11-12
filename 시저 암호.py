6cacf7d46444efa5425881d1ca3c8f46e424604b
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-13 00:37:45 +0900

    Update 시저 암호.py

--- 시저 암호.py
+++ 시저 암호.py
@@ -1,3 +1,4 @@
+"""
 def solution(s, n):
     s = list(s)
     for i in range(len(s)):
@@ -7,3 +8,24 @@ def solution(s, n):
             s[i]=chr((ord(s[i])-ord('a')+ n)%26+ord('a'))
 
     return "".join(s)
+"""
+#2022.11.12
+import re
+
+def shift_char(char, n):
+    if char.islower() and ord(char) + n > ord('z'):
+        return chr(ord(char) + n - 26)
+    if char.isupper() and ord(char) + n > ord('Z'):
+        return chr(ord(char) + n - 26)
+    return chr(ord(char) + n)
+    
+def solution(s, n):
+    answer = ''
+    
+    for char in s:
+        if re.search('[a-zA-z]', char):
+            answer += shift_char(char, n)
+        else:
+            answer += ' '
+            
+    return answer
