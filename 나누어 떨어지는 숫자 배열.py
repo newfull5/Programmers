e48ccdf000efc977aa877b50a0b8f5d6e612b0f0
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-13 01:37:11 +0900

    Update 나누어 떨어지는 숫자 배열.py

--- 나누어 떨어지는 숫자 배열.py
+++ 나누어 떨어지는 숫자 배열.py
@@ -1,3 +1,4 @@
+"""
 def solution(arr, divisor):
     answer = []
     
@@ -9,3 +10,15 @@ def solution(arr, divisor):
         return [-1]
     else:
         return sorted(answer)
+"""
+#2022.11.13
+def solution(arr, divisor):
+    answer = []
+    
+    for val in sorted(arr):
+        if val % divisor == 0:
+            answer.append(val)
+            
+    if not answer:
+        return [-1]
+    return answer
