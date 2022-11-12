8d8c724917be39e28ddbbac341f9b4062739dc0e
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-13 01:40:49 +0900

    Update 같은 숫자는 싫어.py

--- 같은 숫자는 싫어.py
+++ 같은 숫자는 싫어.py
@@ -1,3 +1,4 @@
+"""
 def solution(s):
     arr = []
     arr.append(s[0])
@@ -7,3 +8,12 @@ def solution(s):
             arr.append(i)
             
     return arr
+"""
+#2022.11.13
+def solution(arr):
+    answer = [-100]
+    for val in arr:
+        if val == answer[-1]:
+            continue
+        answer.append(val)
+    return answer[1:]
