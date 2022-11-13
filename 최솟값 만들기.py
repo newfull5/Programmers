9b1e569167c7fffd2c8418fe32174ba857e833a9
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-13 22:44:57 +0900

    Update 최솟값 만들기.py

--- 최솟값 만들기.py
+++ 최솟값 만들기.py
@@ -8,6 +8,7 @@ def solution(A,B):
         
     return sum(temp)
 '''
+'''
 #2020.03.04
 def solution(A,B):
     A.sort()
@@ -17,3 +18,10 @@ def solution(A,B):
     for i in range(0, len(A)):
         temp += A[i]*B[i]
     return temp
+'''
+#2022.11.13
+def solution(A,B):
+    A = sorted(A)
+    B = sorted(B, reverse=True)
+    
+    return sum(a*b for a,b in zip(A,B))
