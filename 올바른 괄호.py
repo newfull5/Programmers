3148637cf7440a03a83053f78aafa0a4d65a60d2
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-13 22:33:11 +0900

    Update 올바른 괄호.py

--- 올바른 괄호.py
+++ 올바른 괄호.py
@@ -23,8 +23,9 @@ def solution(s):
     return len(stack) == 0
 
 
--------------------'''
-2020.02.14
+'''
+'''
+#2020.02.14
 
 def solution(s):
     answer = s[0]
@@ -35,3 +36,21 @@ def solution(s):
             answer += s[i]
     return answer == ''
     
+'''
+#2022.11.13
+from collections import deque
+
+def solution(s):
+    s = deque(list(s))
+    stack = [0]
+    
+    while s:
+        bracket = s.popleft()
+        if bracket == ')' and stack[-1] == '(':
+            stack.pop()
+        else:
+            stack.append(bracket)
+            
+    if stack == [0]:
+        return True
+    return False
