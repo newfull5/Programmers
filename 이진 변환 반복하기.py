ecf4547cdeab0dcffad9c4051987329c87afb414
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-13 22:52:56 +0900

    Update 이진 변환 반복하기.py

--- 이진 변환 반복하기.py
+++ 이진 변환 반복하기.py
@@ -1,3 +1,4 @@
+"""
 def solution(s):
     
     def Recul(s,count,times):
@@ -10,3 +11,17 @@ def solution(s):
         return Recul(s,count,times+1)
 
     return Recul(s,0,0)
+"""
+#2022.11.13
+def dfs(s,times,cnt):
+    cnt += s.count('0')
+    times += 1
+    s = s.replace('0','')
+    
+    if s == '1':
+        return [times, cnt]
+    
+    return dfs(bin(len(s))[2:], times, cnt)
+
+def solution(s):
+    return dfs(s, 0, 0)
