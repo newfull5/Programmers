6eb639d860d10d16524a83cb3a087ce1ea7c5f82
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-13 11:17:55 +0900

    Update 2016년.py

--- 2016년.py
+++ 2016년.py
@@ -1,3 +1,4 @@
+"""
 def solution(a, b):
     day = [0,31,29,31,30,31,30,31,31,30,31,30,31]
     aaa = ['THU','FRI','SAT','SUN','MON','TUE','WED']
@@ -8,5 +9,10 @@ def solution(a, b):
     summ += b 
      
     return aaa[summ % 7]
-    
-    
+"""
+#2022.11.13
+import datetime
+
+def solution(a, b):
+    date = datetime.datetime(2016,a,b).strftime('%A')
+    return date[:3].upper()
