7dea60e59d7c052cc894c931f1887105d1e0e961
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-13 11:22:32 +0900

    Update 폰켓몬.py

--- 폰켓몬.py
+++ 폰켓몬.py
@@ -8,6 +8,7 @@ def solution(nums):
     else:
         return (len(num))
 '''
+'''
 #2020.03.07
 def solution(nums):
     N = len(nums)//2
@@ -17,3 +18,9 @@ def solution(nums):
         return len(nums)
     else:
         return N
+'''
+#2022.11.13
+def solution(nums):
+    n = len(nums) // 2
+    nums = list(set(nums))
+    return min(len(nums), n)
