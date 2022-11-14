6bc111b4120ffa82495d9123b6cd9602fce66787
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-14 12:18:02 +0900

    Update 숫자의 표현.py

--- 숫자의 표현.py
+++ 숫자의 표현.py
@@ -1,3 +1,4 @@
+"""
 def solution(n):
     answer = []
     for i in range(1,n):
@@ -10,3 +11,23 @@ def solution(n):
                 break
                 
     return len(answer)+1
+"""
+#2022.11.14
+def search_maxn(n):
+    for i in range(1, 140):
+        if i*(i+1)/2 > n:
+            return i-1
+
+def solution(n):
+    max_n = search_maxn(n)
+    answer = 1
+    
+    for i in range(2, max_n+1):
+        for j in range(10000):
+            if sum(range(1+j, 1+j+i)) == n:
+                answer += 1
+                break
+            if sum(range(1+j, 1+j+i)) > n:
+                break
+                
+    return answer
