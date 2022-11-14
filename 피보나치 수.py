5274751d9c96363caa15cbc3c1e2994b015db696
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-14 12:22:56 +0900

    Update 피보나치 수.py

--- 피보나치 수.py
+++ 피보나치 수.py
@@ -1,3 +1,4 @@
+'''
 def solution(n):
     fibo = [0,0,1,2]
     
@@ -5,4 +6,12 @@ def solution(n):
         fibo.append(fibo[i-1]+fibo[i-2])
         
     return ( fibo[-1] % 1234567 )
+'''
+#2022.11.14
+def solution(n):
+    fibo = [0,1]
     
+    for i in range(2,n+1):
+        fibo.append(fibo[-1] + fibo[-2])
+        
+    return fibo[-1] % 1234567
