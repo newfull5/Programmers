e1224b1e3696b0e2fdb39927bc9d4bd9d0456bf0
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-14 22:16:26 +0900

    Update 영어 끝말잇기.py

--- 영어 끝말잇기.py
+++ 영어 끝말잇기.py
@@ -19,6 +19,7 @@ def solution(n, words):
     else:
         return [wrong%n,(wrong//n)+1]
 '''
+'''
 #2020.03.04
 def solution(n, words):
     for i in range(1, len(words)):
@@ -27,3 +28,17 @@ def solution(n, words):
                 return [n, (i // n)+1]
             return [(i+1) % n, (i // n)+1]
     return [0,0]
+'''
+#2022.11.14
+import math
+
+def solution(n, words):
+    history = [words[0]]
+    
+    for i in range(1, len(words)):
+        if words[i-1][-1] != words[i][0] or words[i] in history:
+            people = n if (i+1) % n == 0 else (i+1)%n
+            turn = math.ceil((i+1)/n)
+            return [people, turn]
+        history.append(words[i])
+    return [0, 0]
