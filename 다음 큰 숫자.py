0b9f0d48f06cd0b8d243702948f99209e9ee9e7e
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-14 12:25:56 +0900

    Update 다음 큰 숫자.py

--- 다음 큰 숫자.py
+++ 다음 큰 숫자.py
@@ -1,3 +1,4 @@
+'''
 def solution(n):
     a = bin(n)
     
@@ -7,3 +8,11 @@ def solution(n):
             break
             
     return n
+'''
+#2022.11.14
+def solution(n):
+    num_one = bin(n).count('1')
+    for num in range(n+1, 1000000):
+        if bin(num).count('1') == num_one:
+            return num
+        
