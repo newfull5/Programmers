63e082fc5ea40f7bd38c32060f00a59ac9fd9dfe
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-16 22:36:39 +0900

    Update 프린터.py

--- 프린터.py
+++ 프린터.py
@@ -20,6 +20,7 @@ def solution(priorities, location):
         if trashcan[i][1] == location:
             return i+1
 """
+'''
 #2022.6.22
 def solution(priorities, location):
     priorities = [(v,i) for i,v in enumerate(priorities)]
@@ -41,3 +42,30 @@ def solution(priorities, location):
             
         else:
             priorities.append([num,idx])
+'''
+#2022.11.16
+from collections import deque
+
+def solution(priorities, location):
+    cnt = 0
+    answer = [0]*len(priorities)
+    answer[location] = 1
+    answer = deque(answer)
+    queue = deque(priorities)
+    max_num = max(queue)
+    
+    while queue:
+        val = queue.popleft()
+        pri = answer.popleft()
+        
+        if val == max_num:
+            if queue:
+                max_num = max(queue)
+            cnt += 1
+            if pri == 1:
+                return cnt
+        else:
+            queue.append(val)
+            answer.append(pri)
+            
+    return cnt
