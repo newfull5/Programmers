294575b86a0c319a1752730841eb55bfdea77f14
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-24 18:45:13 +0900

    Update [3차] n진수 게임.py

--- [3차] n진수 게임.py
+++ [3차] n진수 게임.py
@@ -1,3 +1,4 @@
+'''
 def Jinbup(number,n):
     T="0123456789ABCDEF"
     i,j=divmod(number,n)
@@ -18,4 +19,36 @@ def solution(n, t, m, p):
         answer += string[i]
         if len(answer) == t:
             return answer
+'''
+#2022.11.24
+def _jinbub(num, n):
+    answer = ''
+    while num > 0:
+        num, remain = divmod(num, n)
+        if remain >= 15 and n >= 15:
+            answer += 'F'
+        elif remain >= 14 and n >= 14:
+            answer += 'E'
+        elif remain >= 13 and n >= 13:
+            answer += 'D'
+        elif remain >= 12 and n >= 12:
+            answer += 'C'
+        elif remain >= 11 and n >= 11:
+            answer += 'B'
+        elif remain >= 10 and n >= 10:
+            answer += 'A'
+        else:
+            answer += str(remain)
+            
+    return answer[::-1]
+        
+
+def solution(n, t, m, p):
+    answer = ''
+    span = '0'+''.join([_jinbub(v, n) for v in range(t*m+1)])
     
+    for i, v in enumerate(span):
+        if i % m == p-1:
+            answer += v
+        if len(answer) == t:
+            return answer
