b53b5711f6d3b616919068df1e715c43acd9c5a9
Sae_Chan_Oh <dhtocks@naver.com>
2022-11-28 19:13:40 +0900

    Update 오픈채팅방.py

--- 오픈채팅방.py
+++ 오픈채팅방.py
@@ -1,3 +1,4 @@
+"""
 def solution(record):
     diction = {}
     answer = []
@@ -15,3 +16,18 @@ def solution(record):
             diction[rec[1]] = rec[2]
 
     return [diction[a]+b for a,b in answer]
+"""
+#2022.11.28
+def solution(records):
+    answer = []
+    
+    user_dict = {record.split()[1]:record.split()[2] for record in records if record.split()[0] != 'Leave'}
+    
+    for record in records:
+        cmd, *user_info = record.split()
+        if cmd == 'Enter':
+            answer.append(f"{user_dict[user_info[0]]}님이 들어왔습니다.")
+        if cmd == 'Leave':
+            answer.append(f"{user_dict[user_info[0]]}님이 나갔습니다.")
+            
+    return answer
