67386b12f8c8507eb1932ac09ad7d70921f26bf3
Sae_Chan_Oh <dhtocks@naver.com>
2024-04-11 19:26:36 +0900

    Update 메뉴 리뉴얼.py

--- 메뉴 리뉴얼.py
+++ 메뉴 리뉴얼.py
@@ -1,3 +1,4 @@
+'''
 from itertools import combinations as C
 
 def solution(orders, course):    
@@ -20,3 +21,17 @@ def solution(orders, course):
         result += [''.join(a) for a in answer]
         
     return sorted(result)
+'''
+from itertools import combinations
+from collections import Counter
+def solution(orders, course):
+    answer = []
+    for k in course:
+        candidates = []
+        for menu_li in orders:
+            for li in combinations(menu_li, k):
+                res = ''.join(sorted(li))
+                candidates.append(res)
+        sorted_candidates = Counter(candidates).most_common()
+        answer += [menu for menu, cnt in sorted_candidates if cnt > 1 and cnt == sorted_candidates[0][1]]
+    return sorted(answer)
