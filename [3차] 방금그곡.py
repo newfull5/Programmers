08a79629796557f70ff3d95eab5a020a35b031c3
Sae_Chan_Oh <dhtocks@naver.com>
2024-04-16 15:31:40 +0900

    Update [3차] 방금그곡.py

--- [3차] 방금그곡.py
+++ [3차] 방금그곡.py
@@ -1,3 +1,4 @@
+'''
 def solution(m, musicinfos):
     def convert(string):
         string = string.replace('C#','c')
@@ -30,3 +31,42 @@ def solution(m, musicinfos):
         return "(None)"
 
     return answer[0][0]
+'''
+import re
+
+
+def replace_note(string):
+    string = string.replace('C#', 'c')
+    string = string.replace('D#', 'd')
+    string = string.replace('F#', 'f')
+    string = string.replace('G#', 'g')
+    string = string.replace('A#', 'a')
+    string = string.replace('B#', 'b')
+    string = string.replace('E#', 'e')
+    return string
+    
+    
+def calc_timestring(time):
+    h,m = time.split(':')
+    return int(h)*60 + int(m)
+
+
+def solution(m, musicinfos):
+    answer = []
+    m = replace_note(m)
+    
+    for idx, musicinfo in enumerate(musicinfos):
+        start, end, name, melody = musicinfo.split(',')
+        duration = calc_timestring(end) - calc_timestring(start)
+        melody = (replace_note(melody)*10000)[:duration]
+        
+        if re.search(m, melody):
+            answer.append([name, duration, idx])
+    
+    if not answer:
+        return '(None)'
+    
+    answer = sorted(answer, key=lambda x: x[-1], reverse=True)
+    answer = sorted(answer, key=lambda x: x[-2])
+    name, *_ = answer.pop()
+    return name
