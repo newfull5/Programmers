30deb2597dba2fd4f09d84d17a6cfc4f9e1e63a5
Sae_Chan_Oh <dhtocks@naver.com>
2024-04-21 15:34:08 +0900

    Update 줄 서는 방법.py

--- 줄 서는 방법.py
+++ 줄 서는 방법.py
@@ -1,3 +1,4 @@
+'''
 from math import factorial as F
 
 def solution(n, k):
@@ -8,6 +9,21 @@ def solution(n, k):
     while n != 0:
         answer.append(arr.pop((k-1) // F(n-1)))
         k %= F(n-1)
+        n -= 1
+        
+    return answer
+'''    
+from math import factorial as fac
+
+def solution(n, k):
+    arr = [i for i in range(1, n + 1)]
+    answer = []
+    
+    while arr:
+        a = (k - 1) // fac(n - 1)
+        answer.append(arr.pop(a))
+        
+        k = k % fac(n - 1)
         n -= 1
         
     return answer
