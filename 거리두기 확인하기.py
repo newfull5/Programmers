1ec816c646d07954431f1da71cedbd84868f4f15
Sae_Chan_Oh <dhtocks@naver.com>
2024-05-01 16:22:46 +0900

    Update 거리두기 확인하기.py

--- 거리두기 확인하기.py
+++ 거리두기 확인하기.py
@@ -1,3 +1,4 @@
+'''
 #2021.07.09
 def solution(places):    
     def up_search(arg):
@@ -102,4 +103,50 @@ def solution(places):
         else:
             answer.append(0)
             
+    return answer
+'''
+
+def check(place): 
+    
+    for i in range(0, 5):
+        for j in range(0, 5):
+            if place[i][j] == 'O': 
+                cnt = 0 
+                if i-1 >= 0:
+                    if place[i-1][j]  == 'P' :
+                        cnt +=1
+                if j-1 >= 0: 
+                    if place[i][j-1] == 'P':
+                        cnt +=1
+                if j+1 <= 4:
+                    if place[i][j+1] == 'P':
+                        cnt +=1
+                if i+1 <= 4:
+                    if place[i+1][j]  == 'P':
+                        cnt +=1
+                
+                if cnt >= 2:
+                    return 0
+            
+            # p바로옆에 p가 있는지 확인
+            if place[i][j] == 'P': 
+                if j+1 <=4 : 
+                    if place[i][j+1] =='P' :
+                        return 0
+                if i+1 <=4 :    
+                    if place[i+1][j] =='P' :
+                        return 0
+                if i-1 >=0 :    
+                    if place[i-1][j] =='P':
+                        return 0 
+                if j-1 >=0 :     
+                    if place[i][j-1] =='P':
+                        return 0
+    return 1
+
+def solution(places):
+    answer = []
+    
+    for i in range(0, len(places)):
+        answer.append(check(places[i]))
     return answer
