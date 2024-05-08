0b1e9701bca6b68e1b126db81a56ffb2f9381d55
Sae_Chan_Oh <dhtocks@naver.com>
2024-05-08 17:14:06 +0900

    Update 문자열 압축.py

--- 문자열 압축.py
+++ 문자열 압축.py
@@ -1,3 +1,4 @@
+'''
 def solution(s):
     result = len(s)
 
@@ -22,3 +23,32 @@ def solution(s):
         result = min(result, len(answer))
 
     return result
+'''
+def solution(s):
+    result=[]
+    
+    if len(s)==1:
+        return 1
+    
+    for i in range(1, len(s)+1):
+        b = ''
+        cnt = 1
+        tmp=s[:i]
+
+        for j in range(i, len(s)+i, i):
+            
+            if tmp==s[j:i+j]:
+                cnt+=1
+            else:
+                if cnt!=1:
+                    b = b + str(cnt)+tmp
+                else:
+                    b = b + tmp
+                    
+                tmp=s[j:j+i]
+                cnt = 1
+                
+        result.append(len(b))
+        
+
+    return min(result)
