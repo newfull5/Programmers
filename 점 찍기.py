00395defa68fdc066b39d03077d0759a76078127
Sae_Chan_Oh <dhtocks@naver.com>
2024-05-12 19:15:30 +0900

    Create 점 찍기.py

---
+++ 점 찍기.py
@@ -0,0 +1,31 @@
+def bisect(x,d):
+    left = 0
+    right = d
+    mid = (left + right) // 2
+    
+    while left<=right:
+        if x**2 + mid**2 == d**2:
+            return [x, mid]
+        
+        if x**2 + mid**2 > d**2 and x**2 + (mid-1) < d**2:
+            return [x, mid-1]
+            
+        if x**2 + mid**2 < d**2:
+            left = mid+1
+            mid = (left + right) // 2
+            continue
+            
+        elif x**2 + mid**2 > d**2:
+            right = mid
+            mid = (left + right) // 2
+            
+def solution(k, d):
+    lists = []
+    for x in range(0, d+1, k):
+        lists.append(bisect(x, d))
+                
+    answer = 0
+    for x,y in lists:
+        answer += (y // k)+1
+        
+    return answer
