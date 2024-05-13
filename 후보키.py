f201f8c827f2d1aff15ae2b1b39e9970a4054209
Sae_Chan_Oh <dhtocks@naver.com>
2024-05-13 17:45:08 +0900

    Update 후보키.py

--- 후보키.py
+++ 후보키.py
@@ -1,3 +1,4 @@
+'''
 from itertools import combinations
 
 def Isunique(lists,answer):
@@ -22,4 +23,44 @@ def solution(relation):
             if Isunique(tup,answer):
                 answer.append(tup)
                 
+    return len(answer)
+'''
+from itertools import combinations as c
+
+
+def check_dup(answer, cols):
+    for ans in answer:
+        for n in ans:
+            if n in cols:
+                continue
+            else:
+                break
+        else:
+            return True
+        
+    return False
+
+def check_valid_key(relation, key):
+    sets = []
+    
+    for rel in relation:
+        sets.append(''.join([rel[i] for i in key]))
+        
+    return len(set(sets)) == len(relation)
+
+
+def solution(relation):
+    answer = []
+    
+    col = [i for i in range(len(relation[0]))]
+    
+    for num_col in range(1, len(col)+1):
+        for key in c(col, num_col):
+            key = list(key)
+            if check_dup(answer, key):
+                continue
+            
+            if check_valid_key(relation, key):
+                answer.append(key)
+            
     return len(answer)
