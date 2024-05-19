1cbc28c9b914e0dc901b0457debdfd278b431481
Sae_Chan_Oh <dhtocks@naver.com>
2024-05-19 16:35:48 +0900

    Update 야근 지수.py

--- 야근 지수.py
+++ 야근 지수.py
@@ -1,3 +1,4 @@
+'''
 #보자마자 힙으로 푸는 문제구나 하고 깨닳았다. 뭔가 조금은 성장한 느낌이다.
 
 from heapq import heappop, heappush
@@ -20,3 +21,17 @@ def solution(n, works):
         answer += i**2
     
     return answer
+'''
+from heapq import heapify, heappush, heappop
+
+def solution(n, works):
+    if sum(works) <= n:
+        return 0
+    
+    works = [-work for work in works]
+    heapify(works)
+    
+    for _ in range(n):
+        heappush(works, heappop(works) + 1)
+        
+    return sum([work**2 for work in works])
