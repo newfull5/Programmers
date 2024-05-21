347f9d2e40922da8dce1c5d7d1fe2f31a7c1d1e2
Sae_Chan_Oh <dhtocks@naver.com>
2024-05-21 14:24:30 +0900

    Update 최고의 집합.py

--- 최고의 집합.py
+++ 최고의 집합.py
@@ -1,3 +1,4 @@
+'''
 def solution(n, s):
     if n > s:
         return [-1]
@@ -6,3 +7,18 @@ def solution(n, s):
     b = [((s//n)+1) for _ in range(s-((s//n)*n))]
 
     return a+b
+'''
+
+from heapq import heappop, heappush, heapify
+
+def solution(n, s):
+    if n > s:
+        return [-1]
+    
+    heap = [int(s/n) for _ in range(n)]
+    heapify(heap)
+    
+    for _ in range(s - sum(heap)):
+        heappush(heap, heappop(heap) + 1)
+        
+    return sorted(heap)
