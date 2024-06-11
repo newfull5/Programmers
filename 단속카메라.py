e2d5d9c50732548243508bfafb54f82f38a648c7
Sae_Chan_Oh <dhtocks@naver.com>
2024-06-11 17:57:26 +0900

    Update 단속카메라.py

--- 단속카메라.py
+++ 단속카메라.py
@@ -1,3 +1,4 @@
+'''
 def solution(routes):
     routes = sorted(routes, key = lambda x: x[-1])
     length = len(routes)
@@ -14,4 +15,29 @@ def solution(routes):
         for j in range(i, length):
             if routes[j][0] <= camera and camera <= routes[j][1]:
                 chk[j] = True
+    return answer
+'''
+
+def check_overlab(locate, route2):
+    left, right = route2
+    if left <= locate <= right:
+        return True
+    return False
+
+def solution(routes):
+    answer = 0
+    routes = sorted(routes, key=lambda x: x[-1])
+    check = [False] * len(routes)
+    
+    for i, route in enumerate(routes):
+        if check[i]:
+            continue
+            
+        answer += 1
+        locate = route[-1]
+        
+        for j, route2 in enumerate(routes):
+            if check_overlab(locate, route2):
+                check[j] = True
+                
     return answer
