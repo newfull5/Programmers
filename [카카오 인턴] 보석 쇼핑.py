ac251b29bd840d802a09004dfacfb5bfaa3521f1
Sae_Chan_Oh <dhtocks@naver.com>
2024-07-16 18:50:04 +0900

    Update [카카오 인턴] 보석 쇼핑.py

--- [카카오 인턴] 보석 쇼핑.py
+++ [카카오 인턴] 보석 쇼핑.py
@@ -1,3 +1,4 @@
+'''
 def solution(gems):
     size = len(set(gems)) 
     dic = {gems[0]:1}
@@ -29,3 +30,26 @@ def solution(gems):
 
     answer = answer[-1]
     return [answer[0]+1, answer[1]+1]
+'''
+def solution(gems):
+    num_kinds = len(set(gems))
+    answer = [0, float('inf')]
+    
+    def check_include_all(l,r):
+        if len(set(gems[l:r])) == num_kinds:
+            return True
+        return False
+    
+    
+    for left in range(0, len(gems)):
+        for right in range(left+1, len(gems)+1):
+            
+            if right - left >= answer[1]-answer[0]:
+                break
+                
+            if check_include_all(left, right):
+                while check_include_all(left, right):
+                    left += 1
+                answer = [left, right]
+        
+    return answer
