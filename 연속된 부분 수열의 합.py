055d5c9245ba799914dca4fd11a8342256ed10fa
Sae_Chan_Oh <dhtocks@naver.com>
2024-07-22 18:30:54 +0900

    Update 연속된 부분 수열의 합.py

--- 연속된 부분 수열의 합.py
+++ 연속된 부분 수열의 합.py
@@ -1,3 +1,4 @@
+'''
 def solution(sequence, k):
     answers = []
     sum = 0
@@ -20,4 +21,29 @@ def solution(sequence, k):
     
     answers.sort(key=lambda x: (x[1]-x[0], x[0]))
     return answers[0]
- 
+'''
+def solution(sequence, k):
+    answer = [0,0]
+    answer_len = 1_000_000
+    
+    for left in range(len(sequence)):
+        for right in range(left+1, len(sequence)+1):
+            window_size = right - left
+            
+            if window_size >= answer_len:
+                break
+            
+            if sum(sequence[left:right]) < k:
+                continue
+                
+            elif sum(sequence[left:right]) > k:
+                break
+                
+            elif sum(sequence[left:right]) == k:
+                if window_size == 1:
+                    return [left, right-1]
+                
+                answer = [left, right-1]
+                answer_len = right-left
+                
+    return answer
